2d22556538091359fb2ba4fcedab5bf8d9a7150d
kikluz <savir81@hotmail.com>
2021-07-22 13:19:16 -0700

    create HomeComponent

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View, Platform } from 'react-native';
 import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';  
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
+
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -25,19 +28,42 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: {screen: Home}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator},
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8ff'
+    }
+)
+const AppNavigator = createAppContainer(MainNavigator)
+// const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
  
     render() {
         return(
 
-            <View 
-                style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
-                }}
-            > 
+            <View  style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+
                 <AppNavigator />
             </View>
         );
