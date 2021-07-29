6dde0efa4aac549cd5214f46ba2119fd5d3a1e2c
kikluz <savir81@hotmail.com>
2021-07-29 00:15:07 -0700

    Setting up Redux

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,6 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
-
+// so far working
 export const fetchComments = () => dispatch => {
     return fetch(baseUrl + 'comments')
         .then(response => {
