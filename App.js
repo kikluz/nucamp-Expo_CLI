b911011474428c9363f8383d8d7625d27d14dce1
kikluz <savir81@hotmail.com>
2021-08-05 18:02:11 -0700

    Persist Redux Store

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
 
-const store = ConfigureStore();
+// Bring both persistor and store using Object destructuring Syntax
+const {persistor, store} = ConfigureStore();
 
 export default function App() {
     return (
         <Provider store={store}>
-            <Main />
+            {/* wrapp main in the PersistGate prevent the redux store to render until has
+            rehydrated fully from the client-site store 
+             */}
+            <PersistGate loading={<Loading />}  persistor={persistor}>
+                <Main />
+            </PersistGate>
         </Provider>
     );
 }
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,10 +6,24 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers  } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+// setup confi with values 
+const  config = {
+    // key property value root 
+    key: 'root',
+    // property of storage 
+    storage,
+    // redux persist to console msg for debug any issues 
+    debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        // Handle update the state to local storage, reducer  is used to update redux store 
+        // and config as the first argumenet 
+        persistCombineReducers(config, {
             campsites,
             comments,
             partners,
@@ -19,5 +33,9 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    // this anable the store to be persisted
+    const persistor = persistStore(store);
+// return an Object that contains both persistor and store
+// so we can access both from App.js 
+    return {persistor ,store}
 }
